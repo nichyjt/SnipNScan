0e2f4ed427c499f442b878a4fa4d6d6c8bbcc95a
nichyjt <nicholas.yek@gmail.com>
2021-12-17 18:12:39 +0800

    add local build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script is meant to simplify debugging and 'automate' compilation locally on a Linux machine.
+if g++ ./src/*.cpp ./src/*.h `wx-config --cxxflags --libs std` `pkg-config --cflags --libs opencv4 tesseract` -o ssout; then
+    ./SnipNScan;
+    du -h SnipNScan; # Show hard disk usage for analysis 
+fi
